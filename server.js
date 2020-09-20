8abf27d1cfadd50c8d2a11f8044096d8b6f39c95
dylan <dylansalim3@hotmail.com>
2020-09-20 12:30:45 +0800

    Added push notification config

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var app = express();
 var port = 5000;
 
 app.use(bodyParser.json());
-app.use(cors({origin:"*",allowedHeaders:"Origin, X-Requested-With, Content-Type, Accept"}));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors({ origin: "*", allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV !== 'production') {
     const dotenv = require('dotenv');
     dotenv.config();
 }
+
 
 var multer = require('multer');
 
@@ -41,9 +42,6 @@ const upload = multer({
     fileFilter: fileFilter
 })
 
-
-// var upload = multer({ dest: 'uploads/' });
-
 const Users = require('./routes/Users');
 const Books = require('./routes/Books');
 const BookDetails = require('./routes/BookDetails');
@@ -53,6 +51,7 @@ const Genres = require('./routes/Genres');
 const Roles = require('./routes/Roles');
 const LibraryMaps = require('./routes/LibraryMaps');
 const BackupDatabase = require('./routes/BackupDatabase');
+const Subscriptions = require('./routes/Subscriptions');
 
 app.use('/uploads', express.static('uploads'));
 app.use('/users', Users);
@@ -63,13 +62,16 @@ app.use('/borrow-books-history', BorrowBooksHistory);
 app.use('/genres', Genres);
 app.use('/roles', Roles);
 app.use('/library-maps', LibraryMaps);
-app.use('/backup-database',BackupDatabase);
+app.use('/backup-database', BackupDatabase);
+app.use('/subscription',Subscriptions);
 
 
 app.post('/file', upload.single('file'), function (req, res, next) {
     const filepath = req.file.path;
     res.send(filepath);
 });
+
+
 
 
 const borrowBook = require('./models/BorrowBook');
@@ -85,34 +87,34 @@ const userRole = require('./models/UserRole');
 const category = require('./models/Category');
 const db = require('./database/db');
 
-book.belongsTo(bookDetail, {foriegnKey: 'book_detail_id', constraint: true, OnDelete: 'CASCADE'});
-bookDetail.hasMany(book, {foriegnKey: 'book_detail_id'});
+book.belongsTo(bookDetail, { foriegnKey: 'book_detail_id', constraint: true, OnDelete: 'CASCADE' });
+bookDetail.hasMany(book, { foriegnKey: 'book_detail_id' });
 
-borrowBook.belongsTo(book, {foreignKey: 'book_id', constraint: true, OnDelete: 'CASCADE'});
-book.hasMany(borrowBook, {foreignKey: 'book_id'});
+borrowBook.belongsTo(book, { foreignKey: 'book_id', constraint: true, OnDelete: 'CASCADE' });
+book.hasMany(borrowBook, { foreignKey: 'book_id' });
 
-borrowBook.belongsTo(user, {foreignKey: 'user_id',});
-user.hasMany(borrowBook, {foreignKey: 'user_id',});
+borrowBook.belongsTo(user, { foreignKey: 'user_id', });
+user.hasMany(borrowBook, { foreignKey: 'user_id', });
 
-borrowBookHistory.belongsTo(book, {foreignKey: 'book_id'});
-book.hasMany(borrowBookHistory, {foreignKey: 'book_id'});
+borrowBookHistory.belongsTo(book, { foreignKey: 'book_id' });
+book.hasMany(borrowBookHistory, { foreignKey: 'book_id' });
 
-borrowBookHistory.belongsTo(user, {foreignKey: 'user_id'});
-user.hasMany(borrowBookHistory, {foreignKey: 'user_id'});
+borrowBookHistory.belongsTo(user, { foreignKey: 'user_id' });
+user.hasMany(borrowBookHistory, { foreignKey: 'user_id' });
 
-bookDetail.belongsTo(genre, {foreignKey: 'genre_id'});
-genre.hasOne(bookDetail, {foreignKey: 'genre_id'});
+bookDetail.belongsTo(genre, { foreignKey: 'genre_id' });
+genre.hasOne(bookDetail, { foreignKey: 'genre_id' });
 
-bookDetail.belongsToMany(author, {through: "book_author", foreignKey: 'book_detail_id'});
-author.belongsToMany(bookDetail, {through: "book_author", foreign_key: 'author_id'});
+bookDetail.belongsToMany(author, { through: "book_author", foreignKey: 'book_detail_id' });
+author.belongsToMany(bookDetail, { through: "book_author", foreign_key: 'author_id' });
 
-user.belongsToMany(role, {through: "user_role", foreignKey: 'user_id'});
-role.belongsToMany(user, {through: "user_role", foreignKey: 'role_id'});
+user.belongsToMany(role, { through: "user_role", foreignKey: 'user_id' });
+role.belongsToMany(user, { through: "user_role", foreignKey: 'role_id' });
 
 bookDetail.hasOne(category);
 category.belongsTo(bookDetail);
 
-db.sequelize.sync({logging:false});
+db.sequelize.sync({ logging: false });
 
 app.listen(port, () => {
     console.log("Server is running on part: " + port)
